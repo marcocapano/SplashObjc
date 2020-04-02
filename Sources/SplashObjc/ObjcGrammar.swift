3a8c94c46c7b4559ef233edb0e77c77862128dd6
Marco Capano <marco24capano@icloud.com>
2020-04-03 00:09:05 +0100

    [ADD] support for numbers

--- Sources/SplashObjc/ObjcGrammar.swift
+++ Sources/SplashObjc/ObjcGrammar.swift
@@ -16,6 +16,8 @@ public struct ObjcGrammar: Grammar {
 
         syntaxRules = [
             StringLiteral(),
+            NumberRule(),
+            CharacterLiteralRule(),
             TypeRule(),
             KeywordRule()
         ]
@@ -91,6 +93,12 @@ public struct ObjcGrammar: Grammar {
                 }
             }
 
+            //Capitalized Words at the beginning of strings and capitalized character literals
+            //Are not to be considered types.
+            guard segment.tokens.previous != "'" && segment.tokens.next != "'" else {
+                return false
+            }
+
             guard let firstCharacter = segment.tokens.current.first.map(String.init) else {
                 return false
             }
@@ -103,5 +111,76 @@ public struct ObjcGrammar: Grammar {
 
             return true
         }
+    }
+
+    struct NumberRule: SyntaxRule {
+        var tokenType: TokenType { return .number }
+
+        func matches(_ segment: Segment) -> Bool {
+            let tokens = segment.tokens
+
+            if tokens.current.isNumber || tokens.current.isNumberObjectLiteral {
+                return true
+            }
+
+            //Floating points numbers come in the format number.number
+            if tokens.current == ".", let previous = tokens.previous, let next = tokens.next {
+                return (previous.isNumber || previous.isNumberObjectLiteral)
+                    && (next.isNumber || next.isNumberObjectLiteral)
+            }
+
+            //@YES and @NO will be highlighter as numbers
+            if tokens.current == "@YES" || tokens.current == "@NO" {
+                return true
+            }
+
+            return false
+        }
+    }
+
+    struct CharacterLiteralRule: SyntaxRule {
+        //Character literals get highlighted the same way as number literals
+        var tokenType: TokenType = .number
+
+        func matches(_ segment: Segment) -> Bool {
+//            if segment.tokens.current == "'" {
+//                return true
+//            }
+
+            return segment.tokens.previous == "'" && segment.tokens.next == "'"
+        }
+    }
+}
+
+extension String {
+    var isNumber: Bool {
+        if hasSuffix("L")
+            || hasSuffix("F")
+            || hasSuffix("f")
+            || hasSuffix("U") {
+            return String(dropLast()).isNumber
+        }
+
+        if hasSuffix("LL") {
+            return String(dropLast(2)).isNumber
+        }
+
+        let validAsInt = Int(self) != nil
+        return validAsInt
+    }
+
+    var isNumberObjectLiteral: Bool {
+        guard hasPrefix("@") else { return false }
+        let withoutAtSign = dropFirst()
+        return String(withoutAtSign).isNumber
+    }
+
+    var isCharacterObjectLiteral: Bool {
+        guard hasPrefix("@") else { return false }
+        let withoutAtSign = String(dropFirst())
+
+        return withoutAtSign.hasPrefix("'")
+            && withoutAtSign.hasSuffix("'")
+            && withoutAtSign.count > 2
     }
 }
--- Tests/SplashObjcTests/LiteralsTests.swift
+++ Tests/SplashObjcTests/LiteralsTests.swift
@@ -35,6 +35,198 @@ class LiteralTests: XCTestCase {
             .plainText(";")
         ])
     }
+
+    func testIntegerLiteral() {
+        let components = highlighter.highlight("int kobe = 24;")
+
+        XCTAssertEqual(components, [
+            .token("int", .keyword),
+            .whitespace(" "),
+            .plainText("kobe"),
+            .whitespace(" "),
+            .plainText("="),
+            .whitespace(" "),
+            .token("24", .number),
+            .plainText(";")
+        ])
+    }
+
+    func testIntegerObjectLiteral() {
+        let components = highlighter.highlight("NSNumber *number = @10;")
+
+        XCTAssertEqual(components, [
+            .token("NSNumber", .type),
+            .whitespace(" "),
+            .plainText("*number"),
+            .whitespace(" "),
+            .plainText("="),
+            .whitespace(" "),
+            .token("@10", .number),
+            .plainText(";")
+        ])
+    }
+
+    func testUnsignedIntegerObjectLiteral() {
+        let components = highlighter.highlight("NSNumber *fortyTwoUnsigned = @42U;")
+
+        XCTAssertEqual(components, [
+            .token("NSNumber", .type),
+            .whitespace(" "),
+            .plainText("*fortyTwoUnsigned"),
+            .whitespace(" "),
+            .plainText("="),
+            .whitespace(" "),
+            .token("@42U", .number),
+            .plainText(";")
+        ])
+    }
+
+    func testLongIntegerObjectLiteral() {
+        let components = highlighter.highlight("NSNumber *fortyTwoLong = @42L;")
+
+        XCTAssertEqual(components, [
+            .token("NSNumber", .type),
+            .whitespace(" "),
+            .plainText("*fortyTwoLong"),
+            .whitespace(" "),
+            .plainText("="),
+            .whitespace(" "),
+            .token("@42L", .number),
+            .plainText(";")
+        ])
+    }
+
+    func testLongLongIntegerObjectLiteral() {
+        let components = highlighter.highlight("NSNumber *fortyTwoLongLong = @42LL;")
+
+        XCTAssertEqual(components, [
+            .token("NSNumber", .type),
+            .whitespace(" "),
+            .plainText("*fortyTwoLongLong"),
+            .whitespace(" "),
+            .plainText("="),
+            .whitespace(" "),
+            .token("@42LL", .number),
+            .plainText(";")
+        ])
+    }
+
+    func testBoolObjectLiteral() {
+        let components = highlighter.highlight("""
+        NSNumber *yesNumber = @YES;
+        NSNumber *noNumber = @NO;
+        """)
+
+        XCTAssertEqual(components, [
+            .token("NSNumber", .type),
+            .whitespace(" "),
+            .plainText("*yesNumber"),
+            .whitespace(" "),
+            .plainText("="),
+            .whitespace(" "),
+            .token("@YES", .number),
+            .plainText(";"),
+            .whitespace("\n"),
+            .token("NSNumber", .type),
+            .whitespace(" "),
+            .plainText("*noNumber"),
+            .whitespace(" "),
+            .plainText("="),
+            .whitespace(" "),
+            .token("@NO", .number),
+            .plainText(";")
+        ])
+    }
+
+    func testDoubleLiteral() {
+        let components = highlighter.highlight("double duration = 2.0;")
+
+        XCTAssertEqual(components, [
+            .token("double", .keyword),
+            .whitespace(" "),
+            .plainText("duration"),
+            .whitespace(" "),
+            .plainText("="),
+            .whitespace(" "),
+            .token("2.0", .number),
+            .plainText(";")
+        ])
+    }
+
+    func testFloatLiteral() {
+        let components = highlighter.highlight("float duration = 2.0f;")
+
+        XCTAssertEqual(components, [
+            .token("float", .keyword),
+            .whitespace(" "),
+            .plainText("duration"),
+            .whitespace(" "),
+            .plainText("="),
+            .whitespace(" "),
+            .token("2.0f", .number),
+            .plainText(";")
+        ])
+    }
+
+    func testDoubleObjectLiteral() {
+        let components = highlighter.highlight("NSNumber *piDouble = @3.14;")
+
+        XCTAssertEqual(components, [
+            .token("NSNumber", .type),
+            .whitespace(" "),
+            .plainText("*piDouble"),
+            .whitespace(" "),
+            .plainText("="),
+            .whitespace(" "),
+            .token("@3.14", .number),
+            .plainText(";")
+        ])
+    }
+
+    func testFloatObject() {
+        let components = highlighter.highlight("NSNumber *piFloat = @3.14F;")
+
+        XCTAssertEqual(components, [
+            .token("NSNumber", .type),
+            .whitespace(" "),
+            .plainText("*piFloat"),
+            .whitespace(" "),
+            .plainText("="),
+            .whitespace(" "),
+            .token("@3.14F", .number),
+            .plainText(";")
+        ])
+    }
+
+    func testCharacterLiteral() {
+        let components = highlighter.highlight("char letter = 'M';")
+
+        XCTAssertEqual(components, [
+            .token("char", .keyword),
+            .whitespace(" "),
+            .plainText("letter"),
+            .whitespace(" "),
+            .plainText("="),
+            .whitespace(" "),
+            .token("'M'", .number),
+            .plainText(";")
+        ])
+    }
+
+    func testCharacterObjectLiteral() {
+        let components = highlighter.highlight("NSNumber *letter = @'M';")
+
+        XCTAssertEqual(components, [
+            .token("NSNumber", .type),
+            .whitespace(" "),
+            .plainText("*letter"),
+            .whitespace(" "),
+            .plainText("="),
+            .whitespace(" "),
+            .token("'M'", .number),
+            .plainText(";")
+        ])
+    }
 }
 
 //MARK: - Mocks
