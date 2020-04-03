3fed2343b19e7b0a7adeae01c20ee7162e519257
Marco Capano <marco24capano@icloud.com>
2020-04-03 16:43:33 +0100

    [ADD] preprocessing tests

--- Tests/SplashObjcTests/PreprocessorTests.swift
+++ Tests/SplashObjcTests/PreprocessorTests.swift
@@ -1,8 +1,47 @@
 //
-//  File.swift
+//  PreprocessorTests.swift
 //  
 //
 //  Created by Marco Capano on 03/04/2020.
 //
 
-import Foundation
+import XCTest
+import Splash
+@testable import SplashObjc
+
+class PreprocessorTests: XCTestCase {
+    private(set) var highlighter: SyntaxHighlighter<OutputFormatMock>!
+    private(set) var builder: OutputBuilderMock!
+
+    override func setUp() {
+        super.setUp()
+        builder = OutputBuilderMock()
+
+        let format = OutputFormatMock(builder: builder)
+        highlighter = SyntaxHighlighter(format: format, grammar: ObjcGrammar())
+    }
+
+    func testDefineNumber() {
+        let components = highlighter.highlight("#define MAX_ARRAY_LENGTH 20")
+
+        XCTAssertEqual(components, [
+            .token("#define", .preprocessing),
+            .whitespace(" "),
+            .token("MAX_ARRAY_LENGTH", .preprocessing),
+            .whitespace(" "),
+            .token("20", .preprocessing)
+        ])
+    }
+
+    func testDefineString() {
+        let components = highlighter.highlight("#define MAX_ARRAY_LENGTH 20")
+
+        XCTAssertEqual(components, [
+            .token("#define", .preprocessing),
+            .whitespace(" "),
+            .token("MAX_ARRAY_LENGTH", .preprocessing),
+            .whitespace(" "),
+            .token("20", .preprocessing)
+        ])
+    }
+}
