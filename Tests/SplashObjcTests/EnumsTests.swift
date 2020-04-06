1621586f662b65da05c11e4e72b967a2f0352f32
Marco Capano <marco24capano@icloud.com>
2020-04-06 10:27:18 +0100

    [ADD] tests for enums

--- Tests/SplashObjcTests/EnumsTests.swift
+++ Tests/SplashObjcTests/EnumsTests.swift
@@ -1,8 +1,81 @@
 //
-//  File.swift
-//  
+//  EnumsTests.swift
 //
 //  Created by Marco Capano on 06/04/2020.
 //
+//  Thanks to https://nshipster.com/ns_enum-ns_options/ for a really useful article on Objective-C enums ❤️
 
-import Foundation
+import XCTest
+import Splash
+@testable import SplashObjc
+
+class EnumsTests: XCTestCase {
+    private(set) var highlighter: SyntaxHighlighter<OutputFormatMock>!
+    private(set) var builder: OutputBuilderMock!
+
+    override func setUp() {
+        super.setUp()
+        builder = OutputBuilderMock()
+
+        let format = OutputFormatMock(builder: builder)
+        highlighter = SyntaxHighlighter(format: format, grammar: ObjcGrammar())
+    }
+
+    func testSimpleIntegerValuesEnum() {
+        let components = highlighter.highlight("""
+        enum {
+            UITableViewCellStyleDefault,
+            UITableViewCellStyleValue1,
+            UITableViewCellStyleValue2,
+            UITableViewCellStyleSubtitle
+        };
+        """)
+
+        XCTAssertEqual(components, [
+            .token("enum", .keyword),
+            .whitespace(" "),
+            .plainText("{"),
+            .whitespace("\n    "),
+            .plainText("UITableViewCellStyleDefault,"),
+            .whitespace("\n    "),
+            .plainText("UITableViewCellStyleValue1,"),
+            .whitespace("\n    "),
+            .plainText("UITableViewCellStyleValue2,"),
+            .whitespace("\n    "),
+            .plainText("UITableViewCellStyleSubtitle"),
+            .whitespace("\n"),
+            .plainText("};")
+        ])
+    }
+
+    func testTypedefEnum() {
+        let components = highlighter.highlight("""
+        typedef enum {
+            UITableViewCellStyleDefault,
+            UITableViewCellStyleValue1,
+            UITableViewCellStyleValue2,
+            UITableViewCellStyleSubtitle
+        } UITableViewCellStyle;
+        """)
+
+        XCTAssertEqual(components, [
+            .token("typedef", .keyword),
+            .whitespace(" "),
+            .token("enum", .keyword),
+            .whitespace(" "),
+            .plainText("{"),
+            .whitespace("\n    "),
+            .plainText("UITableViewCellStyleDefault,"),
+            .whitespace("\n    "),
+            .plainText("UITableViewCellStyleValue1,"),
+            .whitespace("\n    "),
+            .plainText("UITableViewCellStyleValue2,"),
+            .whitespace("\n    "),
+            .plainText("UITableViewCellStyleSubtitle"),
+            .whitespace("\n"),
+            .plainText("}"),
+            .whitespace(" "),
+            .plainText("UITableViewCellStyle;")
+        ])
+    }
+}
